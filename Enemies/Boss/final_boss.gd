e752147c6933ad479c27d6802d8e76a6a5fe0463
Avery <averyjcuevas@gmail.com>
2025-03-23 17:06:12 -0700

    all mobs
    all mobs may now be killed with the sword

--- Enemies/Boss/final_boss.gd
+++ Enemies/Boss/final_boss.gd
@@ -4,9 +4,10 @@ extends CharacterBody2D
 @export var limit = 0.5
 @export var endPoint: Marker2D
 
-@onready var animations = $AnimatedSprite2D
+@onready var animations = $AnimationPlayer
 var startPos
 var endPos
+var isDead: bool = false
 
 func _ready():
 	startPos = position
@@ -25,14 +26,32 @@ func updateVelocity():
 	velocity = moveDirection.normalized() * speed
 
 func updateAnimation():
-	var animationString = "walkUp"
-	if velocity.y > 0:
-		animationString = "walkDown"
-		
-	animations.play(animationString)
-
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+	
+		animations.play("walk" + direction)
 
 func _physics_process(delta):
+	if isDead: return
+	
 	updateVelocity()
 	move_and_slide()
 	updateAnimation()
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	print("FinalBoss hit by:", area.name)
+
+	if not area.is_in_group("player_attack"): 
+		print("Not a valid attack, ignoring")
+		return  # Ignore everything except attacks
+
+	print("Valid attack detected!")  # Debugging: Confirm attack is valid
+	isDead = true
+	animations.play("death")
+	await animations.animation_finished
+	queue_free()
